1afaf3a066a1abca8415eb43dd8a58175f071a5d
Arika Ishinami <delete0093@gmail.com>
2026-01-04 22:31:53 +0900

    add testcase

--- tests/IDeepCloneable.Tests/GlobalNamespaceTests.cs
+++ tests/IDeepCloneable.Tests/GlobalNamespaceTests.cs
@@ -5,3 +5,9 @@ internal partial class SampleClass : IDeepCloneable<SampleClass>
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
 }
+
+internal partial class SampleClass2 : IDeepCloneable<SampleClass2>
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
