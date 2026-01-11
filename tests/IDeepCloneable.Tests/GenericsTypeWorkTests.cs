d0fc83a9e4c74c80c13b054df485442b7897dbd5
Arika Ishinami <delete0093@gmail.com>
2026-01-11 17:56:03 +0900

    feat: implement DeepClone support for generic types and collections

--- tests/IDeepCloneable.Tests/GenericsTypeWorkTests.cs
+++ tests/IDeepCloneable.Tests/GenericsTypeWorkTests.cs
@@ -6,39 +6,23 @@ namespace IDeepCloneable.Tests;
 
 public class GenericsTypeWorkTests
 {
-    //[Fact]
-    //public void DeepClone_IEnumerableCustomType_ClonesCorrectly()
-    //{
-    //    // Arrange: Create a class using IEnumerable
-    //    var original = new ClassWithIEnumerable
-    //    {
-    //        Items = [1, 2, 3, 4, 5],
-    //        CustomItems = new CustomEnumerable<string>(["foo", "bar", "buz"]),
-    //    };
-
-    //    // Act: Clone the object
-    //    var clone = original.DeepClone();
-
-    //    // Assert: Verify it's a different instance
-    //    clone.ShouldNotBeSameAs(original);
-    //    clone.Items.ShouldNotBeSameAs(original.Items);
-    //    clone.CustomItems.ShouldNotBeSameAs(original.CustomItems);
-
-    //    // Verify values
-    //    var cloneList = new List<int>(clone.Items);
-    //    var originalList = new List<int>(original.Items);
-    //    cloneList.ShouldBe(originalList);
-    //}
 }
 
 // TODO
 // [DeepCloneable]
-// public partial class ClassWithIEnumerable<T>
-// {
-//     public IEnumerable<int> Items { get; set; } = [];
-//     public CustomEnumerable<string> CustomItems { get; set; } = new([]);
-//     public MyGenericsClass<T> GenericsProperty { get; set; } = new();
-// }
+public partial class ClassWithIEnumerable<T>
+{
+    public IEnumerable<int> Items { get; set; } = [];
+    public CustomEnumerable<string> CustomItems { get; set; } = new([]);
+}
+
+// TODO
+// [DeepCloneable]
+public partial class ClassWithGenericsPattern<T>
+{
+    public required MyGenericsClass<T> GenericsProperty { get; set; }
+    public required IEnumerable<MyGenericsClass<T>> NestedGenericsPattern { get; set; }
+}
 
 // Custom IEnumerable implementation for testing
 public class CustomEnumerable<T> : IEnumerable<T>
