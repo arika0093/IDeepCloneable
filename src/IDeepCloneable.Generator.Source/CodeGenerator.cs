94d263d30d735df797b40a91222d5a494d8732c8
Arika Ishinami <delete0093@gmail.com>
2026-01-30 23:28:12 +0900

    Implement embedded attribute generation in CloneableGeneratorCore and refactor CodeGenerator to utilize it

--- src/IDeepCloneable.Generator.Source/CodeGenerator.cs
+++ src/IDeepCloneable.Generator.Source/CodeGenerator.cs
@@ -43,13 +43,6 @@ internal class CodeGenerator(CloneableGeneratorOptionsCore options)
 
         try
         {
-            // Generate embbedded attributes
-            var attributesCode = GenerateEmbbedAttributes();
-            if (attributesCode != null)
-            {
-                context.AddSource($"{options.AttributeExportFileName}.g.cs", attributesCode);
-            }
-
             // Generate DeepCloneExtensions.g.cs
             var extensionsCode = GenerateDeepCloneExtensions(classInfos);
             context.AddSource($"{options.ExtensionsExportFileName}.g.cs", extensionsCode);
@@ -106,76 +99,6 @@ internal class CodeGenerator(CloneableGeneratorOptionsCore options)
             """;
         builder.AppendLine(header);
         return builder;
-    }
-
-    private string? GenerateEmbbedAttributes()
-    {
-        if (!options.GenerateAttributes)
-        {
-            return null;
-        }
-
-        var builder = new IndentedStringBuilder();
-        builder = GenerateFileHeader(builder);
-
-        // first, add EmbeddedAttribute
-        builder.AppendLine("""
-            namespace Microsoft.CodeAnalysis
-            {
-                internal sealed partial class EmbeddedAttribute : global::System.Attribute { }
-            }
-        
-            """);
-        
-        if (options.ExportAttributeNamespace != null)
-        {
-            builder.AppendLine($"namespace {options.ExportAttributeNamespace}");
-            builder.AppendLine("{");
-            builder.IncreaseIndent();
-        }
-
-        // next, add [ShallowCloneAttribute] and [CloneIgnoreAttribute]
-        if (!string.IsNullOrEmpty(options.ShallowCloneAttributeName))
-        {
-            builder.AppendLine($$"""
-                /// <summary>
-                /// Marks a field or property to be shallow-copied during DeepClone operations.
-                /// Properties or fields marked with this attribute will have their references copied directly
-                /// without creating a deep clone of the object.
-                /// </summary>
-                [global::System.AttributeUsage(
-                    global::System.AttributeTargets.Property | global::System.AttributeTargets.Field,
-                    Inherited = false,
-                    AllowMultiple = false
-                )]
-                internal sealed class {{options.ShallowCloneAttributeName}} : global::System.Attribute { }
-                
-                """);
-        }
-        if (!string.IsNullOrEmpty(options.CloneIgnoreAttributeName))
-        {
-            builder.AppendLine($$"""
-                /// <summary>
-                /// Marks a field or property to be ignored during DeepClone operations.
-                /// Properties or fields marked with this attribute will not be cloned and will retain their default values.
-                /// </summary>
-                [global::System.AttributeUsage(
-                    global::System.AttributeTargets.Property | global::System.AttributeTargets.Field,
-                    Inherited = false,
-                    AllowMultiple = false
-                )]
-                internal sealed class {{options.CloneIgnoreAttributeName}} : global::System.Attribute { }
-                
-                """);
-        }
-
-        if (options.ExportAttributeNamespace != null)
-        {
-            builder.DecreaseIndent();
-            builder.AppendLine("}");
-        }
-
-        return builder.ToString();
     }
 
     /// <summary>
@@ -806,6 +729,7 @@ internal class CodeGenerator(CloneableGeneratorOptionsCore options)
 
             // Check special types using SpecialTypeInfo (now includes arrays)
             var classInfoForMatching = GetOrCreateMinimalClassInfo(typeFullName, classInfos);
+
             foreach (
                 var (specialTypeInfo, methodName) in from specialTypeInfo in SpecialTypeInfos
                 where specialTypeInfo.IsMatch(classInfoForMatching)
